b76bf806e2059da81890c8ba112e6e27a9b57192
mAtencio27 <mAtencio27@hotmail.com>
2023-04-14 01:48:43 +0900

    IMPLEMENTED:photo upload. Need to write the endpoint to recieve the file and place into the image file

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -4,57 +4,61 @@ import { Link } from 'react-router-dom';
 
 const ImageUpload = () => {
 
-  const [ prompts, setPrompts ] = useState([])
+  const [ prompts, setPrompts ] = useState([]);
+  const [ files , setFiles ] = useState([]);
 
-  const promptTagBuilderA = async () => {
-    console.log(prompts)
-    const promptList = await Promise.all(prompts.map((p)=>{return <div>p.Prompt</div>}));
-    return promptList
-    //return await Promise.all(prompts.map((p)=>{return <div>hello</div>}));
+  //FILE UPLOAD HANDLER
+  const uploadHandler = (e) => {
+    //console.log(e.target.files[0])
+    setFiles([...files, e.target.files[0]])
+    //console.log(files)
   };
 
+  //FILE SUBMIT HANDLER
+  const submitHandler = () => {
+    console.log(files)
+    if(!files[0]){
+      return
+    };
+
+    //fetch(/upload)
+  };
+
+  //THIS WILL BUILD THE DISPLAYED PROMPTS AND GIVE THE DIVS TO UPLOAD PHOTOS
   const promptTagBuilder = () => {
-    console.log(prompts)
-    const promptList = prompts.map((p)=>{return <div>{p.Prompt}</div>});
+    const promptList = prompts.map((p)=>{return <div className='promptCard'>
+      <h3 className='cardName'>{p.Name}</h3>
+      <div className='prompt'>{p.Prompt}</div>
+      <input type='file' onChange={(e)=>{uploadHandler(e)}}></input>
+      <button type='submit' onClick={(e)=>{submitHandler()}}>Submit</button>
+    </div>});
     return promptList
-    //return await Promise.all(prompts.map((p)=>{return <div>hello</div>}));
   };
 
   const fetchPrompts = async(e) => {
     const res = await fetch("/prompts")
     const data = await res.json()
-    //console.log (data)
-    //console.log(data.response)
-    //return data.response
     setPrompts(data.response);
     return
   };
 
   useEffect(()=>{
-    // const dataFetch = async() => {
-    //   let data = fetchPrompts()
-    //   return data
-    //   //console.log(data)
-    // };
-
-    // setPrompts(dataFetch)
-    //dataFetch()
-    //fetchPrompts()
     const display = async() => {
       await fetchPrompts();
     };
-
     display()
   },[])
 
   return (
-    <div>ImageUpload
-      <button value={3} onClick={(e)=>{console.log(promptTagBuilder())}}> Return prompts </button>
+    <div>
+      <h1>Image Prompts</h1>
+      <div className='promptCardContainer'>
+        {prompts ? promptTagBuilder(): "no prompts"}
+      </div>
       <div className='navButtons'>
           <Link to="/">Back</Link>
           <Link to="/Render">Next</Link>
       </div>
-      <div>{prompts ? promptTagBuilder(): "no prompts"}</div>
     </div>
   )
 }
