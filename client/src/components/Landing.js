669c01b7b25d127107d6a4fc25f2eca845f113e9
mAtencio27 <mAtencio27@hotmail.com>
2023-05-09 01:52:12 +0900

    responsive elements

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import triangle from '../Assets/Page1/triangle.png'
 import centerPhoto from '../Assets/Page1/main.png'
+import footer from '../Assets/Page1/footer.png'
 import character from '../Assets/Page1/character.png'
 import Header from "./Header"
 import News from "./News"
@@ -29,17 +30,26 @@ const Landing = () => {
                 <div className="characterContainer">
                     <img className="character" src={character}></img>
                     <div className="conceptStuff">
+                        <div className="generateHead"></div>
                         <h1 className="ecoHead">ECO-POKE CONCEPT</h1>
                         <div className="ecoHead2container">
                             <h2 className="ecoHead2">lorum ipsum more stuff</h2>
                             <h2 className="ecoHead2">lorum ipsum</h2>
                             <h2 className="ecoHead2">lorum ipsum more stuff</h2>
                         </div>
-                        
+                        <div className="ecoParagraphContainer">
+                            <p className="ecoParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend, risus vitae bibendum posuere, sapien mi pretium elit, vel consequat velit ex nec magna.</p>
+                        </div>
+                        <div className="generateButtonContainer">
+                            {/* <button className="generateButton">GENERATE</button> */}
+                            <Link to="/Home" className="generateButton">GENERATE</Link>
+                        </div>
                     </div>
                 </div>
             </div>
-            {/* <Link to="/Home">Next</Link> */}
+            <div className="footerContainer">
+                <img src={footer} className="footer"></img>
+            </div>
         </div>
         </div>)
 }
